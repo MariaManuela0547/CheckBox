3c8e9a50d278ad7e8cf6f60424e64ce9c5de1260
MariaManuela0547 <124294920+MariaManuela0547@users.noreply.github.com>
2023-03-09 15:50:15 -0300

    Add files via upload

---
+++ CheckBoxTest.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+        
+public class CheckBoxTest
+{
+    public static void main ( String[]args)
+    {
+        CheckBoxFrame CheckBoxFrame = new CheckBoxFrame();
+        CheckBoxFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        CheckBoxFrame.setSize(275 , 110);
+        CheckBoxFrame.setVisible(true);
+    }
+}
